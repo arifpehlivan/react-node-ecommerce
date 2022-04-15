0e159881c53e264ef3bdce6673153160f88bbae6
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-15 03:09:41 +0300

    add slider

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id:1,
+        img: "https://cdn.patirti.com/mnresize/325/-/patirti/products/ELB0063Y5033SYH_1.jpeg",
+        title: "SUMMER SALE",
+        desc: "30% OFF FOR NEW ARRIVALS",
+        bg: "#cbc7c4"
+    },
+    {
+        id:2,
+        img: "https://www.gustoeshop.com/productimages/118807/middle/22yg005834_01_2.jpg",
+        title: "WINTER SALE",
+        desc: "30% OFF FOR NEW ARRIVALS",
+        bg: "#E5E6E0"
+    },
+    {
+        id:3,
+        img: "https://www.gustoeshop.com/productimages/118738/middle/22yg005344_15_6.jpg",
+        title: "POPULAR SALE",
+        desc: "30% OFF FOR NEW ARRIVALS",
+        bg: "#EAECE7"
+    }
+]
