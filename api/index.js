3156f859e855dd7f57337a48cd545b77b22a53aa
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-17 02:09:13 +0300

    add authentication

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ mongoose.connect(process.env.MONGODB)
 });
 
 app.use(express.json())
-app.use("/api/user", userRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
     console.log("Backend server running http://localhost:5000");
